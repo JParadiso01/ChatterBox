aae41fbdad9db2c81ce1f4a1e7b32055fedd2dd5
Joseph Paradiso <josephjpara@gmail.com>
2024-09-17 20:49:41 -0400

    made message class and fixed error handling

--- client.py
+++ client.py
@@ -1,41 +1,64 @@
 import socket
 import os 
 import threading
+import argparse
 
-exiting = False
 ENCODING = 'utf-8'
-#messages = [{addr}: {data}]
+EXIT_MSG = 'exit'
+#messages = [Message, ...]
 messages = []
+
+class Message:
+    def __init__(self, data):
+        split_data = str(data, encoding=ENCODING)
+        split_data = split_data.split(":")
+        self.sender = split_data[0]
+        self.data = split_data[1]
+    def __str__(self):
+        return f"\033[38;5;47m{self.sender}\033[38;5;15m:{self.data}"
+    
+
+'''
+def encode_message(data):
+    encoded_data = ''
+    for letter in data:
+        encoded_data += chr(ord(letter) ^ 47)
+    print(encoded_data)
+    return encoded_data
+
+def decode_message(data):
+    decoded_data = ''
+    for letter in data:
+        decoded_data += chr(ord(letter) ^ 47)
+    print(decoded_data)
+    return decoded_data
+'''
 
 def client_receive_message(addr, data):
     global messages
-    data = str(data, encoding=ENCODING)
-    messages.append(data)
+    msg = Message(data)
+    messages.append(msg)
 
 def client_send_message(conn,data):
     conn.sendall(bytes(data,ENCODING))
 
-def print_messages():
-    global exiting
-    for msg in messages:
-        split_msg = msg.split(':')
+def print_messages(size):
+    global messages
+    MISC_LINE_COIUNT = 4 
+    msgs = messages
+    if len(messages) >= size.lines-MISC_LINE_COIUNT:
+        dif = len(messages) - (size.lines-MISC_LINE_COIUNT)
+        msgs = messages[dif:]   
+    for msg in msgs:
 
-        #connection closed
-        if split_msg == ['']:
-            exiting = True
-            print('\033[2J\033[H')
-            exit(0)
-            break
-        
-        f_msg = f'\033[38;5;47m{split_msg[0]}\033[38;5;15m:{split_msg[1]}'
-        print(f_msg)
+        print(msg)
 
 def setup_screen(name):
     size = os.get_terminal_size()
     #clears screen
-    print('\033[2J\033[H\033[1B')
+    print('\033[2J\033[H')
     print(f'\033[1m\033[38;5;87m{name}: \033[38;5;15m\033[22m')
-    print_messages()
+    print_messages(size)
     #brings cursor down and creates prompt
     print(f'\033[H\033[{size.lines-2}E'+'-'*size.columns + '> ', end='', flush=True)
 
@@ -43,13 +66,13 @@ def send_data(conn, name):
     global exiting
     while True:
         msg = input()
-        if exiting:
-            print(f'Exiting {name}')
+        if msg == EXIT_MSG:
+            client_send_message(conn, msg)
+            conn.close()
             exit(0)
         client_send_message(conn, msg)
 
 def client(host='127.0.0.1', port=65432):
-    global exiting
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         try:
             s.connect((host, port))
@@ -60,33 +83,24 @@ def client(host='127.0.0.1', port=65432):
         chatroom_name = str(chatroom_name, encoding=ENCODING)
         input_thread = threading.Thread(target=send_data, args=(s, chatroom_name))
         input_thread.start()
-
         while True:
-            if exiting:
-                print(f'Exiting {chatroom_name}')
-                exit(0)
             try:
                 setup_screen(chatroom_name)
                 data = s.recv(1024)
                 client_receive_message(host, data)
-            except ConnectionResetError:
-                print(f'Server has closed the connection to {chatroom_name}')
-                exiting = True
+            except (ConnectionResetError, ConnectionAbortedError):
+                print(f'Connection to {chatroom_name} has been closed')
                 exit(0)
 
 
 def main():
-    '''
-    host = input("Please enter the host that you want to connect to: ")
-    port = None
-    while port == None:
-        try:
-            port = int(input("Please enter the port that you would like to connect to: "))
-        except:
-            print(f'Please enter a valid port number')
-    client(host, port)
-    '''
-    client()
+    parser = argparse.ArgumentParser(add_help=True, description='Sets up a messaging client')
+    parser.add_argument('-t','--host', action='store',default='127.0.0.1', help='The IP you want the socket to bind to. The default IP is "127.0.0.1"',type=str)
+    parser.add_argument('-p','--port', action='store',default=8080, help='The port you want the socket to bind to. The default port is "8080"',type=int)
+
+    args = parser.parse_args()
+
+    client(args.host, args.port)
 
 if __name__ == '__main__':
     main()
--- server.py
+++ server.py
@@ -1,81 +1,102 @@
-#TODO: Add names instead of IPs (would need preprocessing step before adding them to chat room)
 #TODO: Add encryption to the messages! maybe start out with a simple XOR or something, and then go to like AES/ actual algorithms
-#TODO: Fix exiting from the sever so that error messages do not appear
+#TODO: Implement TLS/SSL support (which would handle encryption...)
+#TODO: Make UI cooler (maybe use those big unicode blocks '█' or those continous lines from unicode)
 
-
+import argparse
 import socket
 import threading
 
+class Message:
+    def __init__(self, addr, data):
+        self.sender = addr
+        self.data = str(data, encoding=ENCODING)
+    def __str__(self):
+        return f"{self.sender}: {self.data}"
+    def message_to_bytes(self):
+        return bytes(f"{self.sender}: {self.data}", ENCODING)
+    
 EXIT_MSG = 'exit'
-CHATROOM_NAME = b'Test'
+DEFAULT_CHATROOM_NAME = b'Chat Room'
 ENCODING = 'utf-8'
-#messages = [{addr}: {data}]
+#messages = [Message, ...]
 messages = []
 #client_info = {(connection_socket,(addr, port)): name}  
 client_info = { }
 
+USRNAME_MSG = Message("SERVER", b"Please enter a username")
+CONNECTED_MSG = Message("SERVER", b"")
+
 def print_message(msg):
-   split_msg = msg.split(':')
-   f_msg = f'\033[38;5;47m{split_msg[1]}\033[38;5;15m:{split_msg[2]}'
-   print(f_msg)
+   print(msg)
     
-def server_receive_message(addr, data):
-    msg = f'{addr}: {str(data, encoding=ENCODING)}'
+def server_receive_message(name, data):
+    msg = Message(name, data)
     print(msg)
     messages.append(msg)
     return msg
 
-def server_send_message(data):
+def server_send_message(msg):
     for conn in client_info:
-        conn[0].sendall(bytes(data,ENCODING))
+        conn[0].sendall(bytes(msg.__str__(),ENCODING))
 
-def server(host='127.0.0.1', port=65432):
+def server(host, port, name):
     with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
         s.bind((host, port))
         s.listen()
         print(f'Server listening on {host}:{port}')
         while True:
             conn, addr = s.accept()
-            thread = threading.Thread(target=handle_client, args=(conn, addr))
+            thread = threading.Thread(target=handle_client, args=(conn, addr, name))
             thread.start()
 
-def handle_client(conn, addr):
+
+def handle_client(conn, addr, name):
     global client_info
 
     #get client info
     connected = True
-    print(f'[INFO]: {addr[0]} has connected on port {addr[1]}')
-    conn.send(CHATROOM_NAME)
-    conn.sendall(b"SERVER: Please enter a username")
-    username = conn.recv(1024)
-    username = str(username, encoding=ENCODING)
-    client_info[(conn, addr)] = username
-    print(f"SERVER: {username} has joined the chat room!")
-    server_send_message(f"SERVER: {username} has joined the chat room!")
+    try:
+        print(f'[INFO]: {addr} has connected')
+        conn.send(bytes(name, ENCODING))
+        conn.sendall(USRNAME_MSG.message_to_bytes())
+        username = conn.recv(1024)
+        username = str(username, encoding=ENCODING)
+        client_info[(conn, addr)] = username
+        CONNECTED_MSG.data = f"{username} has joined the chat room!"
+        print(CONNECTED_MSG)
+        server_send_message(CONNECTED_MSG)
+    except ConnectionResetError:
+        connected = False
+        print(f'[INFO]: {addr} has disconnected')
+        conn.close()
 
     #generic client loop
     while connected:
-        conn_msg = conn.recv(1024)
-        if str(conn_msg, encoding=ENCODING) == EXIT_MSG:
+        try:
+            msg_in_bytes = conn.recv(1024)
+            if str(msg_in_bytes, encoding=ENCODING) == EXIT_MSG:
+                connected = False
+                client_info.pop((conn, addr))
+                print(f'[INFO]: {addr}:{username} has exitted the chat room')
+                print(f'SERVER: {username} has left the chat room')
+                server_send_message(f'SERVER: {username} has left the chat room')
+                break
+            formatted_msg = server_receive_message(client_info[(conn, addr)], msg_in_bytes)
+            server_send_message(formatted_msg)
+        except ConnectionResetError:
             connected = False
             client_info.pop((conn, addr))
-            print(f'[INFO]: {addr} has exitted the chat room')
+            print(f'[INFO]: {addr}:{username} has exitted the chat room')
             print(f'SERVER: {username} has left the chat room')
             server_send_message(f'SERVER: {username} has left the chat room')
             break
-        formatted_msg = server_receive_message(client_info[(conn, addr)], conn_msg)
-        server_send_message(formatted_msg)
     conn.close()
 
 if __name__ == '__main__':
-    '''
-    host = input("Please enter the host that you want to bind to: ")
-    port = None
-    while port == None:
-        try:
-            port = int(input("Please enter the port that you would like to bind to: "))
-        except:
-            print(f'Please enter a valid port number')
-    server(host, port)
-    '''
-    server()
+    parser = argparse.ArgumentParser(add_help=True, description='Sets up a messaging server')
+    parser.add_argument('-t','--host', action='store',default='127.0.0.1', help='The IP you want the socket to bind to. The default IP is "127.0.0.1"',type=str)
+    parser.add_argument('-p','--port', action='store',default=8080, help='The port you want the socket to bind to. The default port is "8080"',type=int)
+    parser.add_argument('-n','--name', action='store',default='Chat Room', help='The name you want the chat room to be titled. The default name is "Chat Room"',type=str)
+    
+    args = parser.parse_args()
+    server(args.host, args.port, args.name)
